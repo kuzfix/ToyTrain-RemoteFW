37e7a0669bd2d2f5708c9ea777e5b0ae6a887b00
kuzfix <matija.pirc@fe.uni-lj.si>
2019-11-30 19:17:05 +0100

    First UNTESTED version with basic functionality the remote should have

--- main.c
+++ main.c
@@ -18,70 +18,114 @@
 #define MAX_RETRIES 200
 int WakeUpReceiver();
 
-int main(void)
+/* PROTOCOL
+Wake - wake up the receiver - no action if already awake
+Fnn! - Forward with speed nn: ASCII 0(stop)-99(max speed)
+Bnn! - Backwards with speed nn (same format as F)
+Tss! - Time to auto stop.
+tss! - Time to auto stop if command receifed frome remote controller
+*/
+
+#define MSG_QUEUE_LENGTH  10
+#define MSG_LENGTH  4
+
+uint8_t MsgQueue[MSG_QUEUE_LENGTH][MSG_LENGTH];
+int MQin;
+int MQout;
+int MQN;
+
+int PushMsg(uint8_t* msg);
+int PullMsg(uint8_t* dst);
+int isMsgQueueFull() {return (MQN >=MSG_QUEUE_LENGTH);}
+int isMsgQueueEmpty() {return (!MQN);}
+int numberOfItemsInMsgQueue() {return MQN;}
+
+int PushMsg(uint8_t* msg)
 {
-  char temp;
-  uint8_t tx_address[5] = {0xE7,0xE7,0xE7,0xE7,0xE7};
-  uint8_t rx_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7};
+  if (isMsgQueueFull()) return 0;
+  memcpy(&MsgQueue[MQin][0],msg,MSG_LENGTH);
+  MQN++;
+  MQin++;
+  if (MQin >= MSG_QUEUE_LENGTH) MQin = 0;
+  return 1;
+}
 
-	KBD_Init();
-	LED_Init();
-	Systime_Init();
-	UART0_Init();
-	stdout=&UART0_str;
-	sei();
+int PullMsg(uint8_t* dst)
+{
+  if (isMsgQueueEmpty()) return 0;
+  memcpy(dst, &MsgQueue[MQout][0],MSG_LENGTH);
+  MQN--;
+  MQout++;
+  if (MQout >= MSG_QUEUE_LENGTH) MQout = 0;
+  return 1;
+}
+
+#define MAX_MSG_SEND_RETRIES  10
+void ProcessMsgQueue()
+{
+  static uint8_t msg[5];  //initialized to zero before main, 5th byte never written (always \o)
+  static int state,cnt;
+  int status;
   
-  nrf24_init();                   // init hardware pins
-  nrf24_config(2,4);              // Channel #2 , payload length: 4
-  nrf24_tx_address(tx_address);   // Set device addresses
-  nrf24_rx_address(rx_address);
-
-	printf("while(1):\r\n");
-  while (1)
+  switch (state)
   {
-    if (HasOneMillisecondPassed())
-    {
-      if (WakeUpReceiver())
+    case 0: //WakeUp the receiver
+      if (!isMsgQueueEmpty())
       {
-        nrf24_send((uint8_t*)"Msg!");
-        while(nrf24_isSending());         // Wait for transmission to end
-        temp = nrf24_lastMessageStatus(); // Make analysis on last transmission attempt
-        if(temp == NRF24_TRANSMISSON_OK)
+        status = WakeUpReceiver();
+        if (status == -1) //ERROR - didn't wake up (report and discard message)
         {
-          temp = nrf24_retransmissionCount();
-          printf(" OK R=%d\r\n",temp);
+          printf("Can't wake it up");
+          PullMsg(msg);          
         }
-        else if(temp == NRF24_MESSAGE_LOST)
+        else if (status == 1) //Receiver woken up
         {
-          printf(" e\r\n");
+          state++;
         }
+        //else: still trying...
       }
-    }
-    /*
-    nrf24_send(data);                  // Automatically goes to TX mode
-    while(nrf24_isSending());         // Wait for transmission to end 
-    
-    temp = nrf24_lastMessageStatus(); // Make analysis on last transmission attempt 
-
-    if(temp == NRF24_TRANSMISSON_OK)
-    {
-      temp = nrf24_retransmissionCount();
-      if (oldSuccess) printf(" %d", temp);
-      else printf("\r\nOK R=%d ",temp);
-      oldSuccess=1;
-    }
-    else if(temp == NRF24_MESSAGE_LOST)
-    {
-      if (!oldSuccess) printf("L");
-      else printf("\r\nL");
-      oldSuccess=0;
-    }
-    
-    nrf24_powerUpRx();  // Optionally, go back to RX mode ...
-    // nrf24_powerDown(); //Or you might want to power down after TX
-
-    _delay_ms(10);*/
+      break;
+    case 1: //Get message
+      if (PullMsg(msg))
+      {
+        state++;
+      }
+      else  //Error - reset state machine
+      {
+        state=0;
+        printf("Couldn't get msg in state 1. Someone stealing them? (%s,L:%d)",__FILE__,__LINE__);
+      }
+      //Do not put a break here!
+    case 2:
+      nrf24_send(msg);
+      cnt = 0;
+      state++;
+      break;
+    case 3:
+      if (nrf24_isSending()) break;
+      status = nrf24_lastMessageStatus();
+      if(status == NRF24_TRANSMISSON_OK)
+      {
+        state = 0;
+        printf("C%d",cnt);
+      }
+      else //if(status == NRF24_MESSAGE_LOST)
+      {
+        cnt++;
+        if (cnt > MAX_MSG_SEND_RETRIES)  //after MAX_MSG_SEND_RETRIES Give up
+        {
+          state = 0;
+          printf("Send failed (%s)",msg);
+        }
+        nrf24_send(msg);
+      }
+      break;
+    default:
+      printf("Unknown state %d (%s,L:%d,%s)",state,__FILE__,__LINE__,__FUNCTION__);
+      state=0;
+      break;
   }
+  
 }
 
 int WakeUpReceiver()
@@ -91,7 +135,7 @@ int WakeUpReceiver()
   static int cnt=0;
   int status=0;
   int result=0;
-    
+  
   switch (state)
   {
     case 0:
@@ -119,11 +163,69 @@ int WakeUpReceiver()
         }
         nrf24_send(data);
       }
-      break;       
+      break;
     default:
-      printf("Unknown state %d",state);
+      printf("Unknown state %d (%s,L:%d,%s)",state,__FILE__,__LINE__,__FUNCTION__);
       state=0;
       break;
   }
   return result;
 }
+
+
+
+int main(void)
+{
+  //char temp;
+  char key;
+  uint8_t tx_address[5] = {0xE7,0xE7,0xE7,0xE7,0xE7};
+  uint8_t rx_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7};
+
+	KBD_Init();
+	LED_Init();
+	Systime_Init();
+	UART0_Init();
+	stdout=&UART0_str;
+	sei();
+  
+  nrf24_init();                   // init hardware pins
+  nrf24_config(2,4);              // Channel #2 , payload length: 4
+  nrf24_tx_address(tx_address);   // Set device addresses
+  nrf24_rx_address(rx_address);
+
+	printf("while(1):\r\n");
+  while (1)
+  {
+    if (HasOneMillisecondPassed())
+    {
+      ProcessMsgQueue();
+      KBD_Read();
+      key = KBD_GetKey();
+      switch (key)
+      {
+        case BTN1: PushMsg((uint8_t*)"F50!"); break;
+        case BTN2: PushMsg((uint8_t*)"F99!"); break;
+        case BTN3: PushMsg((uint8_t*)"F00!"); break;
+        case BTN4: PushMsg((uint8_t*)"B99!"); break;
+        case BTN6: PushMsg((uint8_t*)"B50!"); break;
+      }
+      /*if (WakeUpReceiver())
+      {
+        nrf24_send((uint8_t*)"Msg!");
+        while(nrf24_isSending());         // Wait for transmission to end
+        temp = nrf24_lastMessageStatus(); // Make analysis on last transmission attempt
+        if(temp == NRF24_TRANSMISSON_OK)
+        {
+          temp = nrf24_retransmissionCount();
+          printf(" OK R=%d\r\n",temp);
+        }
+        else if(temp == NRF24_MESSAGE_LOST)
+        {
+          printf(" e\r\n");
+        }
+      }*/
+    }
+  }
+}
+
+
